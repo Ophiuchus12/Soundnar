f73faccc31a5d1d36eed39689da8fd91893c145d
GOISQUE-SARRAZIN CORENTIN <corentin.goisque-sarrazin@etu.unistra.fr>
2025-01-12 12:05:35 +0100

    feat (header) : search and connexion button

--- webMusic/app/routes/albums.tsx
+++ webMusic/app/routes/albums.tsx
@@ -33,38 +33,76 @@ export default function Albums() {
     };
 
     return (
-        <div className="mx-auto px-4 fade-in">
-            {/* Barre de recherche */}
-            <SearchBar value={searchValue} onChange={setSearchValue} placeholder='find your album' />
-            {/* Affichage des résultats de recherche si une recherche est effectuée */}
-            {searchResults &&
-                searchResults.length > 0 ? (
-                <div className="w-full bg-opacity-30 p-6 rounded-lg mb-10">
-                    <h2 className="text-white text-3xl font-semibold text-center mb-6">Search Results</h2>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
-                        {searchResults.map((album: any) => (
-                            <div
-                                key={album.id}
-                                className="text-white text-center flex flex-col items-center hover:scale-105 transition-all duration-300 cursor-pointer"
-                                onClick={() => handleClickAlbum(album.id)}
-                            >
-                                <img
-                                    className="w-50 h-50 object-cover rounded-s border-2 border-opacity-40 border-white mb-2"
-                                    src={album.cover_medium}
-                                    alt={album.title}
-                                />
-                                <p className="font-bold">{album.title}</p>
-                                <p className="text-sm text-gray-400">{album.artist.name}</p>
-                            </div>
-                        ))}
+        <>
+            <header className="bg-gradient-to-r from-purple-600 to-blue-500 text-white p-4 shadow-md">
+                <div className="max-w-7xl mx-auto flex items-center justify-between">
+                    {/* Logo */}
+                    <h1
+                        className="text-3xl font-bold cursor-pointer"
+                        onClick={() => navigate("/")}
+                    >
+                        Soundnar
+                    </h1>
+
+                    {/* Barre de recherche */}
+                    <div className="flex items-center w-full max-w-md mx-4">
+                        <input
+                            type="text"
+                            value={searchValue}
+                            onChange={(e) => setSearchValue(e.target.value)}
+                            placeholder="Find your inspiration"
+                            className="w-full p-2 rounded-lg text-gray-900"
+                        />
                     </div>
                 </div>
-            ) : (
-                <div className="text-center text-gray-400 mt-6">
-                    No album found.
-                </div>
-            )
-            }
-        </div>
+            </header>
+            <div className="mx-auto px-4 fade-in">
+                {/* Affichage des résultats de recherche si une recherche est effectuée */}
+                {searchResults &&
+                    searchResults.length > 0 ? (
+                    <div className="w-full bg-opacity-30 p-6 rounded-lg mb-10">
+                        <h2 className="text-white text-3xl font-semibold text-center mb-6">Search Results</h2>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+                            {searchResults.map((album: any) => (
+                                <div
+                                    key={album.id}
+                                    className="text-white text-center flex flex-col items-center hover:scale-105 transition-all duration-300 cursor-pointer"
+                                    onClick={() => handleClickAlbum(album.id)}
+                                >
+                                    <img
+                                        className="w-50 h-50 object-cover rounded-s border-2 border-opacity-40 border-white mb-2"
+                                        src={album.cover_medium}
+                                        alt={album.title}
+                                    />
+                                    <p className="font-bold">{album.title}</p>
+                                    <p className="text-sm text-gray-400">{album.artist.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ) : (
+                    <div className="text-center mt-12">
+                        <img
+                            src="https://via.placeholder.com/150"
+                            alt="No album illustration"
+                            className="w-40 h-40 mx-auto mb-6 opacity-70"
+                        />
+                        <h2 className="text-2xl text-white font-semibold mb-4">
+                            Oops! No albums found
+                        </h2>
+                        <p className="text-gray-400 mb-6">
+                            Try refining your search or explore our trending albums!
+                        </p>
+                        <button
+                            onClick={() => navigate('/')}
+                            className="bg-purple-600 text-white font-semibold px-6 py-3 rounded-lg shadow hover:bg-purple-700 transition"
+                        >
+                            Explore Trending Albums
+                        </button>
+                    </div>
+                )
+                }
+            </div>
+        </>
     )
 }
--- webMusic/app/routes/artists.tsx
+++ webMusic/app/routes/artists.tsx
@@ -65,85 +65,107 @@ export default function Artists() {
     };
 
     return (
-        <div className="mx-auto px-4 fade-in">
-            {/* Barre de recherche */}
-            <SearchBar value={artistSearch} onChange={setArtistSearch} placeholder='find your artist' />
+        <>
+            <header className="bg-gradient-to-r from-purple-600 to-blue-500 text-white p-4 shadow-md">
+                <div className="max-w-7xl mx-auto flex items-center justify-between">
+                    {/* Logo */}
+                    <h1
+                        className="text-3xl font-bold cursor-pointer"
+                        onClick={() => navigate("/")}
+                    >
+                        Soundnar
+                    </h1>
 
-            {/* Affichage des résultats de recherche si une recherche est effectuée */}
-            {searchResults ? (
-                searchResults.length > 0 ? (
-                    <div className="w-full bg-opacity-30 p-6 rounded-lg mb-10">
-                        <h2 className="text-white text-3xl font-semibold text-center mb-6">Search Results</h2>
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
-                            {searchResults.map((artist: any) => (
-                                <div
-                                    key={artist.id}
-                                    className="text-white text-center flex flex-col items-center hover:scale-105 transition-all duration-300 cursor-pointer"
-                                    onClick={() => handleClickArtist(artist.id)}
-                                >
-                                    <img
-                                        className="w-50 h-50 object-cover rounded-full border-2 border-opacity-40 border-white mb-2"
-                                        src={artist.picture_medium}
-                                        alt={artist.name}
-                                    />
-                                    <p className="font-bold">{artist.name}</p>
-                                    <p className="text-sm text-gray-400">Artist</p>
-                                </div>
-                            ))}
-                        </div>
+                    {/* Barre de recherche */}
+                    <div className="flex items-center w-full max-w-md mx-4">
+                        <input
+                            type="text"
+                            value={artistSearch}
+                            onChange={(e) => setArtistSearch(e.target.value)}
+                            placeholder="Find your inspiration"
+                            className="w-full p-2 rounded-lg text-gray-900"
+                        />
                     </div>
-                ) : (
-                    <div className="text-center text-gray-400 mt-6">
-                        No artists available.
-                    </div>
-                )
-            ) : (
-                /* Si aucune recherche n'est effectuée, afficher le menu des genres et les artistes */
-                <>
-                    <div className="flex justify-center items-center mt-6">
-                        <label className="relative mr-4">
-                            <select
-                                value={idGenres}
-                                onChange={(e) => setIdGenres(parseInt(e.target.value, 10))}
-                                className="bg-gray-800 text-white py-2 px-4 rounded-lg border border-gray-600 shadow-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition duration-300 ease-in-out hover:bg-gray-700"
-                            >
-                                {genres.map((genre) => (
-                                    <option key={genre.id} value={genre.id}>
-                                        {genre.name}
-                                    </option>
+                </div>
+            </header>
+            <div className="mx-auto px-4 fade-in">
+
+                {/* Affichage des résultats de recherche si une recherche est effectuée */}
+                {searchResults ? (
+                    searchResults.length > 0 ? (
+                        <div className="w-full bg-opacity-30 p-6 rounded-lg mb-10">
+                            <h2 className="text-white text-3xl font-semibold text-center mb-6">Search Results</h2>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+                                {searchResults.map((artist: any) => (
+                                    <div
+                                        key={artist.id}
+                                        className="text-white text-center flex flex-col items-center hover:scale-105 transition-all duration-300 cursor-pointer"
+                                        onClick={() => handleClickArtist(artist.id)}
+                                    >
+                                        <img
+                                            className="w-50 h-50 object-cover rounded-full border-2 border-opacity-40 border-white mb-2"
+                                            src={artist.picture_medium}
+                                            alt={artist.name}
+                                        />
+                                        <p className="font-bold">{artist.name}</p>
+                                        <p className="text-sm text-gray-400">Artist</p>
+                                    </div>
                                 ))}
-                            </select>
-                        </label>
-                    </div>
-
-                    {/* Liste des artistes */}
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mt-8">
-                        {loading ? (
-                            <div className="text-center text-gray-400">Chargement...</div>
-                        ) : artists.length === 0 ? (
-                            <div className="text-center text-gray-400">
-                                Aucun artiste disponible pour le moment.
                             </div>
-                        ) : (
-                            artists.map((artist) => (
-                                <div
-                                    key={artist.id}
-                                    className="p-4 rounded-lg shadow-md hover:shadow-2xl transition duration-300 ease-in-out hover:scale-105  cursor-pointer"
-                                    onClick={() => handleClickArtist(artist.id)}
+                        </div>
+                    ) : (
+                        <div className="text-center text-gray-400 mt-6">
+                            No artists available.
+                        </div>
+                    )
+                ) : (
+                    /* Si aucune recherche n'est effectuée, afficher le menu des genres et les artistes */
+                    <>
+                        <div className="flex justify-center items-center mt-6">
+                            <label className="relative mr-4">
+                                <select
+                                    value={idGenres}
+                                    onChange={(e) => setIdGenres(parseInt(e.target.value, 10))}
+                                    className="bg-gray-800 text-white py-2 px-4 rounded-lg border border-gray-600 shadow-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500 transition duration-300 ease-in-out hover:bg-gray-700"
                                 >
-                                    <img
-                                        src={artist.picture_medium}
-                                        alt={artist.name}
-                                        className="rounded-full mb-4 w-full object-cover border-2 border-purple-500/50 border-blur-lg"
-                                    />
-                                    <h2 className="text-white text-lg font-bold text-center">{artist.name}</h2>
+                                    {genres.map((genre) => (
+                                        <option key={genre.id} value={genre.id}>
+                                            {genre.name}
+                                        </option>
+                                    ))}
+                                </select>
+                            </label>
+                        </div>
+
+                        {/* Liste des artistes */}
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mt-8">
+                            {loading ? (
+                                <div className="text-center text-gray-400">Chargement...</div>
+                            ) : artists.length === 0 ? (
+                                <div className="text-center text-gray-400">
+                                    Aucun artiste disponible pour le moment.
                                 </div>
-                            ))
-                        )}
-                    </div>
-                </>
-            )}
-        </div>
+                            ) : (
+                                artists.map((artist) => (
+                                    <div
+                                        key={artist.id}
+                                        className="p-4 rounded-lg shadow-md hover:shadow-2xl transition duration-300 ease-in-out hover:scale-105  cursor-pointer"
+                                        onClick={() => handleClickArtist(artist.id)}
+                                    >
+                                        <img
+                                            src={artist.picture_medium}
+                                            alt={artist.name}
+                                            className="rounded-full mb-4 w-full object-cover border-2 border-purple-500/50 border-blur-lg"
+                                        />
+                                        <h2 className="text-white text-lg font-bold text-center">{artist.name}</h2>
+                                    </div>
+                                ))
+                            )}
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
     );
 
 }    
